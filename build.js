4c64c8ca496b228e7bda2f393e44bf328b84092a
spences10 <spences10apps@gmail.com>
2018-12-22 07:04:55 +0000

    bundle card and remove prod deps

---
+++ build.js
@@ -0,0 +1,67 @@
+'use strict'
+
+// Pull in our modules
+const chalk = require('chalk')
+const boxen = require('boxen')
+const fs = require('fs')
+const path = require('path')
+
+// Define options for Boxen
+const options = {
+  padding: 1,
+  margin: 1,
+  borderStyle: 'round'
+}
+
+// Text + chalk definitions
+const data = {
+  name: chalk.white('Scott Spence /'),
+  handle: chalk.cyan('spences10'),
+  work: chalk.white('Engineer in Creative Products at Karmarama'),
+  twitter: chalk.cyan('https://twitter.com/ScottDevTweets'),
+  github: chalk.cyan('https://github.com/spences10'),
+  linkedin: chalk.cyan(
+    'https://www.linkedin.com/in/linkedinscottspence/'
+  ),
+  web: chalk.cyan('https://scottspence.me'),
+  npx: chalk.white('npx spences10'),
+  labelWork: chalk.white.bold('      Work:'),
+  labelTwitter: chalk.white.bold('   Twitter:'),
+  labelGitHub: chalk.white.bold('    GitHub:'),
+  labelLinkedIn: chalk.white.bold('  LinkedIn:'),
+  labelWeb: chalk.white.bold('       Web:'),
+  labelCard: chalk.white.bold('      Card:')
+}
+
+// Actual strings we're going to output
+const newline = '\n'
+const heading = `${data.name} ${data.handle}`
+const working = `${data.labelWork}  ${data.work}`
+const twittering = `${data.labelTwitter}  ${data.twitter}`
+const githubing = `${data.labelGitHub}  ${data.github}`
+const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
+const webing = `${data.labelWeb}  ${data.web}`
+const carding = `${data.labelCard}  ${data.npx}`
+
+// Put all our output together into a single variable so we can use boxen effectively
+const output =
+  heading +
+  newline +
+  newline +
+  working +
+  newline +
+  twittering +
+  newline +
+  githubing +
+  newline +
+  linkedining +
+  newline +
+  webing +
+  newline +
+  newline +
+  carding
+
+fs.writeFileSync(
+  path.join(__dirname, 'bin/output'),
+  chalk.green(boxen(output, options))
+)
