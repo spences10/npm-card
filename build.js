d710a2f2742b3ebe6e6706a7d46a45eee2b4fca6
spences10 <spences10apps@gmail.com>
2020-01-21 11:20:13 +0000

    update links

--- build.js
+++ build.js
@@ -1,26 +1,26 @@
-'use strict'
+'use strict';
 
 // Pull in our modules
-const chalk = require('chalk')
-const boxen = require('boxen')
-const fs = require('fs')
-const path = require('path')
+const chalk = require('chalk');
+const boxen = require('boxen');
+const fs = require('fs');
+const path = require('path');
 
 // Define options for Boxen
 const options = {
   padding: 1,
   margin: 1,
   borderStyle: 'round',
-}
+};
 
 // Text + chalk definitions
 const data = {
   name: chalk.white('Scott Spence /'),
   handle: chalk.cyan('spences10'),
-  work: chalk.white('Engineer in Creative Products at Karmarama'),
-  twitter: chalk.cyan('https://twitter.com/spences10'),
-  github: chalk.cyan('https://github.com/spences10'),
-  linkedin: chalk.cyan('https://www.linkedin.com/in/spences10/'),
+  work: chalk.white('Web Developer for Creative Products at Karmarama'),
+  twitter: chalk.cyan('https://ss10.dev/twitter'),
+  github: chalk.cyan('https://ss10.dev/git'),
+  linkedin: chalk.cyan('https://ss10.dev/li'),
   web: chalk.cyan('https://scottspence.me'),
   npx: chalk.white('npx spences10'),
   labelWork: chalk.white.bold('      Work:'),
@@ -29,17 +29,17 @@ const data = {
   labelLinkedIn: chalk.white.bold('  LinkedIn:'),
   labelWeb: chalk.white.bold('       Web:'),
   labelCard: chalk.white.bold('      Card:'),
-}
+};
 
 // Actual strings we're going to output
-const newline = '\n'
-const heading = `${data.name} ${data.handle}`
-const working = `${data.labelWork}  ${data.work}`
-const twittering = `${data.labelTwitter}  ${data.twitter}`
-const githubing = `${data.labelGitHub}  ${data.github}`
-const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
-const webing = `${data.labelWeb}  ${data.web}`
-const carding = `${data.labelCard}  ${data.npx}`
+const newline = '\n';
+const heading = `${data.name} ${data.handle}`;
+const working = `${data.labelWork}  ${data.work}`;
+const twittering = `${data.labelTwitter}  ${data.twitter}`;
+const githubing = `${data.labelGitHub}  ${data.github}`;
+const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`;
+const webing = `${data.labelWeb}  ${data.web}`;
+const carding = `${data.labelCard}  ${data.npx}`;
 
 // Put all our output together into a single variable so we can use boxen effectively
 const output =
@@ -57,9 +57,9 @@ const output =
   webing +
   newline +
   newline +
-  carding
+  carding;
 
 fs.writeFileSync(
   path.join(__dirname, 'bin/output'),
   chalk.yellow(boxen(output, options))
-)
+);
